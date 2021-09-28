762da23547290080cc82aaded16eb2ae19da8a84
Allam Nikhil Sai <2019198@iiitdmj.ac.in>
2021-09-28 18:35:29 +0530

    Update StudentLogin.jsx

--- src/react-qr-attendance/components/StudentLogin.jsx
+++ src/react-qr-attendance/components/StudentLogin.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from 'react-bootstrap'
 import { Redirect } from 'react-router-dom'
 import { useAuth } from "../Context"
