424c58d954bc76fd462a2b1f02cdf030681f12e2
Allam Nikhil Sai <2019198@iiitdmj.ac.in>
2021-09-28 17:00:52 +0530

    Update SecureRoute.jsx

--- src/react-qr-attendance/components/SecureRoute.jsx
+++ src/react-qr-attendance/components/SecureRoute.jsx
@@ -1,27 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Redirect, Route} from 'react-router-dom'
 import {useAuth} from '../Context'
 
 export default function SecureRoute({component: Component, role, ...rest}) {
   const {currentUser} = useAuth()
-  const permittedRole =  localStorage.getItem('role')
-  if(currentUser){
-    if(permittedRole === role){
-      return (
-        <Route
-          {...rest}
-          render={props => {
-            return (
-              <Component {...props} /> 
-            ) 
-          }}
-        ></Route>
-      )
-    }
-    else{
-      return <Redirect to='/'/>
-    }
-  }else{
-    return <Redirect to="/" />
-  }
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        return currentUser ? <Component {...props} /> : <Redirect to="/" />
+      }}
+    ></Route>
+  )
 }
