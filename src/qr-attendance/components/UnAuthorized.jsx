691f2c023df55b7128024377c32a272076ed7aca
Allam Nikhil Sai <2019198@iiitdmj.ac.in>
2021-09-29 22:31:36 +0530

    Update UnAuthorized.jsx

--- src/qr-attendance/components/UnAuthorized.jsx
+++ src/qr-attendance/components/UnAuthorized.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import NavBar from './NavBar'
 
 export default function UnAuthorized() {
   return (
     <>
-      <Container style={{height: "90vh"}} fluid className="d-flex justify-content-center align-items-center">
+      <NavBar/>
+      <Container style={{height: "70vh"}} fluid className="d-flex justify-content-center align-items-center">
         <div className="heading breakpoint">
           <div className="react-heading">
             You don't have access to this route!!
