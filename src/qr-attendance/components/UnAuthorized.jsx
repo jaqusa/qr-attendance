c873f8c154b1bd4d304370aa1bb1e74d243603ce
Niku_419 <2019198@iiitdmj.ac.in>
2021-09-29 23:07:12 +0530

    :chart_with_upwards_trend: adding features and navbar to unauthorized route

--- src/qr-attendance/components/UnAuthorized.jsx
+++ src/qr-attendance/components/UnAuthorized.jsx
@@ -5,13 +5,8 @@ import NavBar from './NavBar'
 export default function UnAuthorized() {
   return (
     <>
-<<<<<<< HEAD
       <Container style={{height: "75vh"}} fluid className="d-flex justify-content-center align-items-center">
         <NavBar show={true}/>
-=======
-      <NavBar/>
-      <Container style={{height: "70vh"}} fluid className="d-flex justify-content-center align-items-center">
->>>>>>> 691f2c023df55b7128024377c32a272076ed7aca
         <div className="heading breakpoint">
           <div className="react-heading">
             You don't have access to this route!!
