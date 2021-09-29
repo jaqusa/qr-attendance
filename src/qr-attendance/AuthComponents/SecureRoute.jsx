e5bd7108592dddcaa2868af8ac370384d1e211a5
Niku_419 <2019198@iiitdmj.ac.in>
2021-09-29 23:02:05 +0530

    :tada: made changes in routing

---
+++ src/qr-attendance/AuthComponents/SecureRoute.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import {Redirect, Route} from 'react-router-dom'
+import {useAuth} from '../Context'
+import UnAuthorized from '../components/UnAuthorized'
+
+export default function SecureRoute({component: Component, currentRole, ...rest}) {
+  const { currentUser } = useAuth()
+  const [role, setRole] = useState("")
+  useEffect(() => {
+    setRole(localStorage.getItem("role"))
+  }, [])
+
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        return currentUser ? (
+          (role === currentRole) ? <Component {...props} /> : <UnAuthorized/>
+        ) : <Redirect to="/" />
+      }}
+  ></Route>
+  )
+}
